4c863f63cbc8c2c0e286dd4c2abcace3f941592e
1earthling <stephan@sochoux.com>
2024-09-16 13:21:38 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,32 +1,17 @@
-import boto3
-import warnings
-import urllib3
-
-# Suppress SSL warnings
-warnings.filterwarnings("ignore", category=urllib3.exceptions.InsecureRequestWarning)
-
-def list_s3_buckets_and_files():
-    # Disable SSL verification
-    s3_client = boto3.client('s3', verify=False)
-    s3_resource = boto3.resource('s3', verify=False)
-
-    buckets = s3_client.list_buckets()
-
-    for bucket in buckets['Buckets']:
-        bucket_name = bucket['Name']
-        print(f"Bucket: {bucket_name}")
-        
-        bucket_resource = s3_resource.Bucket(bucket_name)
-        folders = {}
-
-        for obj in bucket_resource.objects.all():
-            folder = '/'.join(obj.key.split('/')[:-1])
-            if folder not in folders:
-                folders[folder] = 0
-            folders[folder] += 1
-
-        for folder, count in folders.items():
-            print(f"  Folder: {folder}, File Count: {count}")
-
-if __name__ == "__main__":
-    list_s3_buckets_and_files()
+osascript -e '
+tell application "Notes"
+    set theFolder to "~/Desktop/ExportedNotes/"
+    set theAccounts to every account
+    repeat with theAccount in theAccounts
+        set theFolders to folders of theAccount
+        repeat with theFolderItem in theFolders
+            set theNotes to notes of theFolderItem
+            repeat with theNote in theNotes
+                set noteTitle to the name of theNote
+                set noteContent to the body of theNote
+                set filePath to theFolder & (noteTitle as string) & ".txt"
+                do shell script "echo " & quoted form of noteContent & " > " & quoted form of filePath
+            end repeat
+        end repeat
+    end repeat
+end tell'
