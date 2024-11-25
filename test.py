7024d358671eaa0e5baa47ef738f0e1321a1e28e
1earthling <stephan@sochoux.com>
2024-11-25 15:18:05 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
 aws cloudtrail lookup-events --lookup-attributes AttributeKey=EventSource,AttributeValue=signin.amazonaws.com
+
+
+aws cloudtrail lookup-events \
+    --lookup-attributes AttributeKey=EventName,AttributeValue=GetCallerIdentity \
+    --start-time "$(date -u -v-1H '+%Y-%m-%dT%H:%M:%SZ')" \
+    --end-time "$(date -u '+%Y-%m-%dT%H:%M:%SZ')"
