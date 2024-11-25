bdad922681a886faf5f33ff9f2cd4525b9bcfb6c
1earthling <stephan@sochoux.com>
2024-11-25 14:40:30 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,1 @@
-osascript -e '
-tell application "Notes"
-    set theFolder to (POSIX path of (path to desktop folder)) & "ExportedNotes/"
-    set theAccounts to every account
-    repeat with theAccount in theAccounts
-        set theFolders to folders of theAccount
-        repeat with theFolderItem in theFolders
-            set theNotes to notes of theFolderItem
-            repeat with theNote in theNotes
-                set noteTitle to the name of theNote
-                set noteContent to the body of theNote
-                set filePath to theFolder & (noteTitle as string) & ".txt"
-                do shell script "echo " & quoted form of noteContent & " > " & quoted form of filePath
-            end repeat
-        end repeat
-    end repeat
-end tell'
+aws cloudtrail lookup-events --lookup-attributes AttributeKey=EventSource,AttributeValue=signin.amazonaws.com
