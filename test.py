5054b85218e02e483c170ab0ef8f7ee45bb86614
1earthling <stephan@sochoux.com>
2024-11-25 15:19:25 -0800

    Update test.py

--- test.py
+++ test.py
@@ -5,3 +5,9 @@ aws cloudtrail lookup-events \
     --lookup-attributes AttributeKey=EventName,AttributeValue=GetCallerIdentity \
     --start-time "$(date -u -v-1H '+%Y-%m-%dT%H:%M:%SZ')" \
     --end-time "$(date -u '+%Y-%m-%dT%H:%M:%SZ')"
+
+
+aws cloudtrail lookup-events \
+    --lookup-attributes AttributeKey=EventName,AttributeValue=GetCallerIdentity \
+    --start-time "$(date -u --date='1 hour ago' +'%Y-%m-%dT%H:%M:%SZ')" \
+    --end-time "$(date -u +'%Y-%m-%dT%H:%M:%SZ')"
